0716321e5f0828108adb3cbd691c783a5ffe0575
Null7 <woran.gao@mail.utoronto.ca>
2017-10-14 04:38:39 -0400

    add comments

--- caesar26/caesar26.py
+++ caesar26/caesar26.py
@@ -1,11 +1,12 @@
-''' encrypts the plaintext with a key
+
+def encrypt(key, plaintext):
+    ''' encrypts the plaintext with a key
     based on the caesar cipher algorithm
     and return the ciphertext
     (string, string) -> string
     REQ: key matches [0-9]*
     REQ: plaintext matches [a-z]*
-'''
-def encrypt(key, plaintext):
+    '''
     encrypted = ''
     for i in plaintext:
         encrypted +=  encrypt_help(key, i)
@@ -25,13 +26,15 @@ def encrypt_help(key, plaintext):
     ciphertext = chr((ord(plaintext) - 96 + int(key)) % 26 + 96)
     return ciphertext
 
-''' decrypts the ciphertext with a key
+ 
+def decrypt_help(key, ciphertext):
+    ''' decrypts the ciphertext with a key
     based on the caesar cipher algorithm
     and returns the plaintext
     (string, string) -> string
     REQ: key matches [0-9]*
-    REQ: ciphertext matches [a-z]*'''    
-def decrypt_help(key, ciphertext):
+    REQ: ciphertext matches [a-z]*
+    ''' 
     if(ciphertext == ''):
         return ''
     if((ord(ciphertext) - 96 - int(key)) % 26 == 0):
